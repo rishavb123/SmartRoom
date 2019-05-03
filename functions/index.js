cc1fe1db4c05080d400188bad0d18a520727dba7
Rishav Bhagat <rishavb123@gmail.com>
2019-05-02 23:09:57 -0400

    made url verification function

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,19 @@
 const functions = require('firebase-functions');
+const express = require('express');
 const cors = require('cors');
-const app = require('express')();
+const bodyParser = require('body-parser');
+
+const app = express();
 
 app.use(cors({ origin: true }));
 
-app.post('/slack/msg', (req, res) => {
-    res.send();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/receive', (req, res) => {
+    res.setHeader('Content-type', 'text/plain');
+    res.statusCode = 200;
+    res.send(req.body.challenge);
 });
 
-exports.widgets = functions.https.onRequest(app);
+exports.slack = functions.https.onRequest(app);
